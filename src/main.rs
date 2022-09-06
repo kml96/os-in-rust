bae392eccedac384cf3fd952dd7a8edbdc2e9359
Kamal Chandra Upreti <kamalchettri1996@gmail.com>
2022-09-06 15:22:37 +0545

    Define custom entry point instead of main function

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 //disable std library
 #![no_std]
+// don't use main() function as entry point
+#![no_main]
 
 // import necessary items
 use core::panic::PanicInfo;
 
 // function to handle panic
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> !  {
+fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
-
-fn main() {}
-
+// entry point for our program
+// disable function name mangling               
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
