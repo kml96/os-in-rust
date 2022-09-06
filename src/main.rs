7f7238d83dc7d095152569c95243f16c8b26ba99
Kamal Chandra Upreti <kamalchettri1996@gmail.com>
2022-09-06 15:02:02 +0545

    Add panic handler and disable std lib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+//disable std library
+#![no_std]
+
+// import necessary items
+use core::panic::PanicInfo;
+
+// function to handle panic
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> !  {
+    loop {}
 }
+
+fn main() {}
+
